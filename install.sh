1b96a17cdbbcf0056de7bf61234334332660cd26
John Novak <john@johnnovak.net>
2016-01-18 21:19:08 +1000

    Installer improvements & correct .config dir handling

--- install.sh
+++ install.sh
@@ -3,18 +3,25 @@
 PWD=`pwd`
 
 create_symlinks() {
-  find $1 -maxdepth 2 -name _\* | sed 's#\(.*/\)_\(.*\)#'$PWD/'\1_\2 '$HOME'/.\2#' | xargs -n 2 ln -s -f
+  find $1 -maxdepth $2 -name _\* | sed 's#\(.*/\)_\(.*\)#'$PWD/'\1_\2 '$3'\2#' | xargs -n 2 ln -s -f
 }
 
-create_symlinks common
+create_platform_symlinks() {
+  create_symlinks $1 1 $HOME/.
+  create_symlinks $1/config 1 $HOME/.config/
+}
+
+create_symlinks common 2 $HOME/.
+
+mkdir -p $HOME/.config
 
 if [[ "$OSTYPE" == 'darwin'* ]]; then
-  create_symlinks osx
+  create_platform_symlinks osx
 
 elif [[ "$OSTYPE" == 'linux'* ]]; then
-  create_symlinks linux
+  create_platform_symlinks linux
 
 elif [[ "$OSTYPE" == 'cygwin'* ]]; then
-  create_symlinks cygwin
+  create_platform_symlinks cygwin
 fi
 
