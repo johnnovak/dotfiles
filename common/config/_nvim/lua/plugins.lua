3b933c3ca3b3bd1a06ee623262ca0af45334b6f1
John Novak <jnovak@skedulo.com>
2022-06-11 10:24:24 +1000

    vim: switch to nvim-cmp

--- common/config/_nvim/lua/plugins.lua
+++ common/config/_nvim/lua/plugins.lua
@@ -35,7 +35,15 @@ return require('packer').startup(
     use 'ctrlpvim/ctrlp.vim'
 
     -- autocomplete
-    use 'hrsh7th/nvim-compe'
+    use {
+		'hrsh7th/nvim-cmp',
+		requires = {
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-buffer',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-cmdline'
+		}
+	}
 
     -- LSP
     use 'neovim/nvim-lspconfig'
